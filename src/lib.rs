38363dbd0a89b6b55da632bc1dc8b008ac70184c
Chunchunmaru142 <127693567+Chunchunmaru142@users.noreply.github.com>
2024-07-21 23:50:08 -0700

    Fixing the colored problem

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
-use colored::Colorize;
+pub extern crate colored;
 
 #[macro_export]
 macro_rules! print_err {
     ($error:expr) => {
+        use handle_errors::colored::Colorize;
         println!(
             "\n{} {} {} {}{}",
             "Error:".red(),
             $error,
             file!().red(),
-            "line:".red(),
-            format!("{}", line!()).red()
+            "line: ".red(),
+            line!()
         )
     };
 }
