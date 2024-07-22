7502eb411cf5d65eb7e4a8aa6a44b5c674ad580d
Chunchunmaru142 <127693567+Chunchunmaru142@users.noreply.github.com>
2024-07-21 22:48:02 -0700

    Base macro

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#[macro_export]
+macro_rules! print_err {
+    ($error:expr) => {
+        println!(
+            "\n{} {} {} {}{}",
+            "Error:".red(),
+            $error,
+            file!().red(),
+            "line:".red(),
+            format!("{}", line!()).red()
+        )
+    };
+}
